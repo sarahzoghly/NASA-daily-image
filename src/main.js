aa1e51d9f1b7de94c3833aa90d5fc9d9b603c7f9
sarahzoghly <sarahzoghly1223@gmail.com>
2026-08-07 22:44:47 +0300

    automated today's date to be datepicker's max

--- src/main.js
+++ src/main.js
@@ -57,4 +57,14 @@ document.querySelector("#datepicker").addEventListener("change", () => {
   reload(date);
 });
 
+function getTodayString() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+document.querySelector("#datepicker").max = getTodayString();
+
 
