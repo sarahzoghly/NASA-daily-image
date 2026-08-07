9de8e1ee9c4d489b8fbb718f10bda1df812152ac
sarahzoghly <sarahzoghly1223@gmail.com>
2026-08-07 19:48:17 +0300

    workings fetch and displayin image or vid correctly

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-document.querySelector("#app").innerHTML = "<h1>it works</h1>";
+const date = document.querySelector("#datepicker").value;
+
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+
+document.querySelector("#app").innerHTML = "<h1>Just a sec..</h1>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
+then(response => response.json()).then(data => {
+    document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
+    if (data.media_type === "image") {
+        document.querySelector("#image").innerHTML = `<img src="${data.url}" alt="${data.title}">`;
+    }
+    else if (data.media_type === "video") {
+        document.querySelector("#image").innerHTML = `<video src="${data.url}" controls></video>`;
+    }
+    else {
+        document.querySelector("#image").innerHTML = `<iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+    }
+    document.querySelector("#description").innerHTML = `<p>${data.explanation}</p>`;
+}).catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
