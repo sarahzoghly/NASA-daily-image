54dbf813d62f42d6364e37b6dc0c218f47934333
sarahzoghly <sarahzoghly1223@gmail.com>
2026-08-07 22:13:14 +0300

    Style the website

--- src/main.js
+++ src/main.js
@@ -1,24 +1,55 @@
-const date = document.querySelector("#datepicker").value;
+import './style.css'
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
-
 document.querySelector("#app").innerHTML = "<h1>Just a sec..</h1>";
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
-then(response => response.json()).then(data => {
-    document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
-    if (data.media_type === "image") {
-        document.querySelector("#image").innerHTML = `<img src="${data.url}" alt="${data.title}">`;
+function reload(date){
+    if (date){
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`).
+        then(response => response.json()).then(data => {
+            document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
+            if (data.media_type === "image") {
+                document.querySelector("#image").innerHTML = `<img src="${data.url}" alt="${data.title}">`;
+            }
+            else if (data.media_type === "video") {
+                document.querySelector("#image").innerHTML = `<video src="${data.url}" controls></video>`;
+            }
+            else {
+                document.querySelector("#image").innerHTML = `<iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+            }
+            document.querySelector("#description").innerHTML = `<p>${data.explanation}</p>`;
+        }).catch(err => {
+            document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+        });
     }
-    else if (data.media_type === "video") {
-        document.querySelector("#image").innerHTML = `<video src="${data.url}" controls></video>`;
+    else{
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
+        then(response => response.json()).then(data => {
+            document.querySelector("#app").innerHTML = `<h1>${data.title}</h1>`;
+            if (data.media_type === "image") {
+                document.querySelector("#image").innerHTML = `<img src="${data.url}" alt="${data.title}">`;
+            }
+            else if (data.media_type === "video") {
+                document.querySelector("#image").innerHTML = `<video src="${data.url}" controls></video>`;
+            }
+            else{
+                document.querySelector("#image").innerHTML = `<iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+            }
+            document.querySelector("#description").innerHTML = `<p>${data.explanation}</p>`;
+
+        }).catch(err => {
+            document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+            document.querySelector("#image").innerHTML = "";
+            document.querySelector("#description").innerHTML = "";
+        })
     }
-    else {
-        document.querySelector("#image").innerHTML = `<iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
-    }
-    document.querySelector("#description").innerHTML = `<p>${data.explanation}</p>`;
-}).catch(err => {
-    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+}
+
+reload()
+
+document.querySelector("#datepicker").addEventListener("change", () => {
+  const date = document.querySelector("#datepicker").value;
+  reload(date);
 });
+
